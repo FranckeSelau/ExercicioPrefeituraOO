a1fd3f1c5bc19a1780a380bf5d3a88c975d53375
Francke Selau <francke.selau@gmail.com>
2015-06-25 22:34:41 -0300

    Create Pessoa.java

---
+++ Pessoa.java
@@ -0,0 +1,39 @@
+package Controle;
+
+public class Pessoa {
+	
+	private int Idade;
+	private String Nome;
+	private char Sexo;
+	private double Salario;
+	
+	
+	public int getIdade() {
+		return Idade;
+	}
+	public void setIdade(int idade) {
+		Idade = idade;
+	}
+	
+	public String getNome() {
+		return Nome;
+	}
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+	
+	public char getSexo() {
+		return Sexo;
+	}
+	public void setSexo(char sexo) {
+		Sexo = sexo;
+	}
+	
+	public double getSalario() {
+		return Salario;
+	}
+	public void setSalario(double salario) {
+		Salario = salario;
+	}
+
+}
